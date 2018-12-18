96d26748195f4b968224d9ae105a30030c98ca35
Christian Martorano <christiansmartorano@gmail.com>
2018-12-18 19:47:34 +0000

    Ajustes no mail controller

--- controller/mail_controller.py
+++ controller/mail_controller.py
@@ -10,6 +10,8 @@ except Exception as e:
 
 import re
 
+import os
+
 from helper import prog_bar
 
 def mail_capture(args):
@@ -22,27 +24,54 @@ def mail_capture(args):
 
     with requests.Session() as s:
         try:
-            r = s.get(url, headers=headers)
+            request = s.get(url, headers=headers)
         except Exception as e:
             print("Erro na requisição GET!")
 
-        email = BeautifulSoup(r.text, 'html.parser').find('input', {'class':'adres-input'}).get('value')
+        email = BeautifulSoup(request.text, 'html.parser').find('input', {'class':'adres-input'}).get('value')
 
         print('E-mail para utilizar: {0}'.format(email))
 
         while True:
             prog_bar.prog_bar_show(args.seg)
-            r = s.get(url, headers=headers)
-            emails = BeautifulSoup(r.text, 'html.parser').findAll(id=re.compile('^(mail_)\d{8,}$'))
+            request = s.get(url, headers=headers)
+            emails = BeautifulSoup(request.text, 'html.parser').findAll(id=re.compile('^(mail_)\d{8,}$'))
             for e in emails:
                 #Acessando os e-mails
                 #print(e)
                 print('{}\nRemetente: {} - Assunto: {}'.format(email, e.find('div', {'class':'gonderen'}).getText(), e.find('div', {'class':'baslik'}).getText()))
 
                 try:
-                    e_request = s.get(e.find('a')['href'])
+                    request = s.get(e.find('a')['href'], headers=headers)
                 except Exception as e:
                     print('Erro na requisição GET!')
 
-                file_download = open('tmp{0}{}{0}{}.html'.format(os.sep, email,
-                                e.find('a')['href'].rstrip('/').split('/')[-1]), 'wb').write(e_request.content)
+                html_page = BeautifulSoup(request.text, 'html.parser')
+
+                try:
+                    request = s.get(html_page.find('iframe')['src'], headers=headers)
+                except Exception as e:
+                    print('Erro na requisição GET!')
+
+                if not os.path.exists('tmp{0}{1}'.format(os.sep, email)):
+                    os.makedirs('tmp{0}{1}'.format(os.sep, email))
+
+
+                file_download = open('tmp{0}{1}{0}{2}.txt'.format(os.sep, email,
+                                html_page.find('iframe')['src'].split('&')[-1]), 'w').write(BeautifulSoup(request.text,
+                                'html.parser').find('div',{'dir':'ltr'}).get_text())
+
+                #Excluir e-mail
+                params = html_page.find('button',{'onclick':re.compile('^(sil_posta\\(")\\w{1,}(",")\\d{1,}("\\));$')})['onclick'].replace('"',
+                                '').split('(')[1].replace(');','').split(',')
+
+
+                data = {
+                    'oturum':html_page.find('iframe')['src'].split('/?')[-1].split('&')[0].split('=')[-1],
+                    'veri[]':[params[0],params[1]]
+                }
+
+                try:
+                    s.post('{}api/sil/'.format(url), data=data, headers=headers)
+                except Exception as e:
+                    print('Erro na requisição POST!')
